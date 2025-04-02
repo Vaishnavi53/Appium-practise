80e2fde1b425cccb3ef9205126720b4c10c2e6b7
Vaishnavi M <149357291+Vaishnavi53@users.noreply.github.com>
2025-04-02 14:48:30 +0530

    Update AppInvoke.cs

--- AppiumPractise/AppWebElements/AppInvoke.cs
+++ AppiumPractise/AppWebElements/AppInvoke.cs
@@ -30,7 +30,7 @@ namespace AppiumPractise.AppWebElements
                 DeviceName = "emulator-5554",
 
             };
-
+             driverOptions.AddAdditionalAppiumOption("avd", "medium_phone_api_35");
             //Initializign appium server 
             //desired capabilities
             driverOptions.AddAdditionalAppiumOption("Application", appPath);
